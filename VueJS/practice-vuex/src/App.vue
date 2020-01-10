f4180a9ad5539502bc34df6b2ed0ffbc148b9bf1
RyuK-H <ryugihyeok@gmail.com>
2020-01-10 19:42:42 +0900

    ENH: Practice Event Bus

--- VueJS/practice-vuex/src/App.vue
+++ VueJS/practice-vuex/src/App.vue
@@ -1,63 +1,68 @@
 <template>
   <div id="app">
-    <Header
-      :new-message-count="newMessageCount"
-    />
-    <div class="container mx-auto mt-5">
-      <ChatList
-        :chat-list="chatList"
-        @read-item="readChatItem"
-      />
-    </div>
+    <button type="button" @click="makeEvent"> 이벤트 생성 </button>
   </div>
 </template>
 <script>
-import Header from './components/Header';
-import ChatList from './components/ChatList';
-export default {
-  name: 'app',
-  data() {
-    return {
-      chatList: [
-        {
-          id: 1,
-          lastMessage: '채팅 메시지1',
-          new: 1
-        },
-        {
-          id: 2,
-          lastMessage: '채팅 메시지2',
-          new: 2
-        },
-        {
-          id: 3,
-          lastMessage: '채팅 메시지3',
-          new: 1
-        },
-        {
-          id: 4,
-          lastMessage: '채팅 메시지4',
-          new: 5
-        }
-      ]
-    };
-  },
-  computed: {
-    newMessageCount() {
-      return this.chatList.map(item => item.new).reduce((a, b) => a + b);
-    }
-  },
-  methods: {
-    readChatItem(chatItem) {
-      this.chatList.filter(item => item.id === chatItem.id)[0].new = 0;
-    }
-  },
-  components: {
-    Header,
-    ChatList
-  }
-}
-</script>
+  import Vue from 'vue';
+  const bus = new Vue();
 
-<style>
-</style>
+  export default {
+    methods: {
+      makeEvent() {
+        bus.$emit('BUTTON-CLICK', 'data');
+      }
+    },
+    created() {
+      bus.$on('BUTTON-CLICK', data => {
+        // eslint-disable-next-line no-console
+        console.log(data);
+      })
+    }
+  }
+// import Header from './components/Header';
+// import ChatList from './components/ChatList';
+// export default {
+//   name: 'app',
+//   data() {
+//     return {
+//       chatList: [
+//         {
+//           id: 1,
+//           lastMessage: '채팅 메시지1',
+//           new: 1
+//         },
+//         {
+//           id: 2,
+//           lastMessage: '채팅 메시지2',
+//           new: 2
+//         },
+//         {
+//           id: 3,
+//           lastMessage: '채팅 메시지3',
+//           new: 1
+//         },
+//         {
+//           id: 4,
+//           lastMessage: '채팅 메시지4',
+//           new: 5
+//         }
+//       ]
+//     };
+//   },
+//   computed: {
+//     newMessageCount() {
+//       return this.chatList.map(item => item.new).reduce((a, b) => a + b);
+//     }
+//   },
+//   methods: {
+//     readChatItem(chatItem) {
+//       this.chatList.filter(item => item.id === chatItem.id)[0].new = 0;
+//     }
+//   },
+//   components: {
+//     Header,
+//     ChatList
+//   }
+// }
+</script>
