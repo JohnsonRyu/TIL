927a6a959542f093b638be99517e1c1cab301ca2
RyuK-H <ryugihyeok@gmail.com>
2020-01-13 18:11:15 +0900

    ENH: Add StartGame Function - CountDown

--- VueJS/rock-scissor-paper/app.js
+++ VueJS/rock-scissor-paper/app.js
@@ -2,5 +2,20 @@ new Vue({
   el: "#app",
   data: {
     myChoice: null,
-  }
+    count: 3
+  },
+  methods: {
+    startGame: function() {
+      if(this.myChoice) {
+        let countDown = setInterval(() => {
+          this.count --;
+          if(this.count === 0) {
+            clearInterval(countDown);
+          }
+        }, 1000);
+      } else {
+        alert("가위 바위 보 중 하나를 선택해주세요.")
+      }
+    }
+  },
 });
