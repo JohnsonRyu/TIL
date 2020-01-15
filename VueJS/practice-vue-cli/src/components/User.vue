8d57f299979db29c6b5a1c700e73511eae872e83
RyuK-H <ryugihyeok@gmail.com>
2020-01-16 01:06:54 +0900

    ENH: Props 활용

--- VueJS/practice-vue-cli/src/components/User.vue
+++ VueJS/practice-vue-cli/src/components/User.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="blue lighten-3 pa-3">
     <h1>User 컴포넌트</h1>
-    <p>이름: 뷰제이에스</p>
+    <p>이름: {{ name }}</p>
+    <v-btn color="info" @click="changeName">버튼</v-btn>
     <hr>
     <v-layout row wrap>
       <v-flex xs12 sm6>
-        <UserDetail></UserDetail>
+        <UserDetail :userName="name"></UserDetail>
       </v-flex>
       <v-flex xs12 sm6>
-        <UserEdit></UserEdit>
+        <UserEdit/>
       </v-flex>
     </v-layout>
   </div>
@@ -22,6 +23,16 @@ export default {
   components: {
     UserDetail,
     UserEdit
-  }
+  },
+  data() {
+    return {
+      name: "기혁"
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = "류기혁";
+    }
+  },
 }
 </script>
--- VueJS/practice-vue-cli/src/components/UserDetail.vue
+++ VueJS/practice-vue-cli/src/components/UserDetail.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
-    <p>상세사항</p>
+    <p>{{ sayHello }}</p>
   </div>
 </template>
+<script>
+  export default {
+    props: {
+      userName: {
+        type: String,
+        default: "LEGO"
+      }
+    },
+    computed: {
+      sayHello() {
+        return this.userName + " 안녕하세요."
+      }
+    }
+  }
+</script>
