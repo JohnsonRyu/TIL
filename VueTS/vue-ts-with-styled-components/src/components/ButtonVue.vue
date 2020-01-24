b044f76dd6193d20ea036345ef71adc0aa09305c
RyuK-H <ryugihyeok@gmail.com>
2020-01-24 15:34:24 +0900

    ENH: Button Component vue 로 제작

---
+++ VueTS/vue-ts-with-styled-components/src/components/ButtonVue.vue
@@ -0,0 +1,55 @@
+<template>
+  <ButtonVue :type="type" :radius="radius">
+    <slot />
+  </ButtonVue>
+</template>
+<script>
+import styled from "vue-styled-components";
+
+const typeButton = {
+  type: String,
+  radius: Boolean
+};
+
+const styleButton = type => {
+  switch (type) {
+    case "primary":
+      return `
+                background-color: #FFFFFF;
+                color: #0057AA;
+            `;
+    case "error":
+      return `
+                background-color: #B4000B;
+                color: #FDFDFD;
+            `;
+    case "success":
+      return `
+            background-color: #00C887;
+            color: #37435F;
+        `;
+    default:
+      return `
+                background-color: #0057AA;
+                color: #FFFFFF;
+            `;
+  }
+};
+
+const ButtonVue = styled("button", typeButton)`
+  font-size: 1em;
+  text-align: center;
+  padding: 10px 15px;
+  border-radius: ${({ radius }) => (radius ? "6px" : null)};
+  ${props => styleButton(props.type)}
+`;
+
+export default {
+  name: "ButtonVue-Template",
+  props: {
+    type: String,
+    radius: Boolean
+  },
+  components: { ButtonVue }
+};
+</script>
