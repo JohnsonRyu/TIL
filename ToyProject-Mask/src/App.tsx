16bd809881886185457af930224119312416956a
RyuK-H <ryugihyeok@gmail.com>
2020-03-02 23:03:51 +0900

    ENH: 프로젝트 세팅

---
+++ ToyProject-Mask/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Provider } from "mobx-react";
+import { Route, BrowserRouter } from "react-router-dom";
+
+import { TestPage } from "./pages";
+import { rootStore } from "./store";
+
+const App = () => (
+  <BrowserRouter>
+    <Provider {...rootStore}>
+      <Route exact path="/" component={TestPage} />
+    </Provider>
+  </BrowserRouter>
+);
+
+export default App;
