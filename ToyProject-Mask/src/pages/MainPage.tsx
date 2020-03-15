52fcf756302405a7010d2eef7c05a606a14a2392
RyuK-H <ryuk@bel.studio>
2020-03-15 23:34:49 +0900

    ENH: Grid 활용한 정렬

--- ToyProject-Mask/src/pages/MainPage.tsx
+++ ToyProject-Mask/src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Statistic } from 'semantic-ui-react'
+import { Grid, Header } from 'semantic-ui-react'
 import { observable } from "mobx";
 import { observer } from "mobx-react";
 import axios from "axios";
@@ -12,10 +12,7 @@ export class MainPage extends Component {
   componentDidMount() {
     axios.get(`http://slb-3774813.ncloudslb.com/`)
       .then(res => {
-        console.error(res.data)
-
         this.curData = res.data.datas;
-
       })
   }
 
@@ -23,22 +20,24 @@ export class MainPage extends Component {
     return(
       this.curData.map((data: any) => {
         return (
-          <Statistic>
-            <Statistic.Value>{data.key}</Statistic.Value>
-            <Statistic.Label>{data.value}</Statistic.Label>
-          </Statistic>
+          <Grid.Row centered>
+            <Grid.Column textAlign="center">
+              <Header as="h2">{data.key}</Header>
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <Header as="h4">{data.value}</Header>
+            </Grid.Column>
+          </Grid.Row>
         )
       })
     )
   }
 
-  
-
   render() {
     return (
-      <Statistic.Group>
+      <Grid columns={3} divided>
         {this.curData ? this.renderData() : null}
-      </Statistic.Group>
+      </Grid>
     );
   }
 }
